06102adacf5d9957be753bbbccacae9712615e76
Anastasia Gurevich <ngurevich23@mail.ru>
2015-08-30 19:21:08 +0400

    initial commit

---
+++ source/directives/slickgrig.js
@@ -0,0 +1,28 @@
+(function(angular, module){
+    'use strict';
+
+    module.directive('slickgrid', function(){
+
+        function linkFunction(scope, el, attr) {
+            var grid = new Slick.Grid("#slickgrid", scope.gridData, scope.gridColumns, scope.gridOptions);
+
+            scope.$on('update-grid-data', function(event, columns, data) {
+                grid.setColumns(columns);
+                grid.setData(data);
+                grid.render();
+            });
+        }
+
+        return {
+            restrict: 'AE',
+            replace: true,
+            template: '<div id="slickgrid" style="width:80%;height:600px; border: 1px solid black"></div>',
+            scope: {
+                gridData : '=',
+                gridColumns: '=',
+                gridOptions : '='
+            },
+            link: linkFunction
+        }
+    });
+})(angular, angular.module('slickgridDemo'));
---
+++ source/main.js
@@ -0,0 +1,58 @@
+'use strict';
+
+angular.module('slickgridDemo', []);
+
+angular.module('slickgridDemo')
+	.controller('gridCtrl', ['$scope', function($scope) {
+
+		function generateColumns(colNum) {
+			var cols = [];
+			var col;
+			for (var i = 0; i < colNum; i++){
+				col = {
+					field: 'data_' + i,
+					id: 'data_' + i, 
+					name: "Data " + i, 
+					visible: true,
+	    			sortable: true,
+	    			width: 100
+				}
+			cols.push(col);
+			}
+			return cols;
+		}
+
+		function generateData(rowNum, columns) {
+			var data = [];
+			for (var i = 0; i < rowNum; i++) {
+				data[i] = {};
+				for (var j =0; j < columns.length; j++){
+					console.log(Math.random());
+					data[i][columns[j].field] = Math.random().toFixed(9);
+				}
+			}
+			return data;
+		}
+
+			$scope.gridRowsNumber = 100;
+			$scope.gridColumnsNumber = 20;
+			$scope.gridOptions = {
+					enableCellNavigation: true,
+    			enableColumnReorder: false
+					};
+
+function init(){
+				$scope.gridColumns = generateColumns($scope.gridColumnsNumber);
+			$scope.data = generateData($scope.gridRowsNumber, $scope.gridColumns);
+}
+
+
+init();
+
+
+		$scope.buildGrid  = function() {
+			init();
+			$scope.$broadcast('update-grid-data', $scope.gridColumns, $scope.data);
+		}
+
+	}]);
